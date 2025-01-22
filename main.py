0d9d2b3816511def6bc50c9539d7b54a7abadc1d
xcczach <2867389537@qq.com>
2025-01-22 04:24:46 +0000

    feat: init on expose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from ml_web_inference import expose, Request, StreamingResponse, get_proper_device
+from ml_web_inference import expose, Request, StreamingResponse
 import torch
 import io
 import argparse
@@ -22,6 +22,7 @@ from speech_tokenizer.utils import extract_speech_token
 from flow_inference import AudioDecoder
 import soundfile as sf
 import setproctitle
+
 
 # 定义 TokenStreamer 类
 class TokenStreamer(BaseStreamer):
@@ -58,6 +59,7 @@ class TokenStreamer(BaseStreamer):
         else:
             return value
 
+
 # 全局变量
 model = None
 glm_tokenizer = None
@@ -66,6 +68,7 @@ whisper_model = None
 feature_extractor = None
 device = None
 model_size_mb = 35000
+
 
 async def inference(request: Request) -> StreamingResponse:
     data = await request.json()
@@ -77,7 +80,9 @@ async def inference(request: Request) -> StreamingResponse:
         temp_audio_path = temp_audio_file.name
         sf.write(temp_audio_path, data=audio_data, samplerate=sample_rate)
         # 提取语音标记
-        audio_tokens = extract_speech_token(whisper_model, feature_extractor, [temp_audio_path], f"cuda:{device}")[0]
+        audio_tokens = extract_speech_token(
+            whisper_model, feature_extractor, [temp_audio_path], f"cuda:{device}" if type(device) == int else device
+        )[0]
     if len(audio_tokens) == 0:
         raise Exception("No audio tokens extracted")
     audio_tokens = "".join([f"<|audio_{x}|>" for x in audio_tokens])
@@ -102,12 +107,12 @@ async def inference(request: Request) -> StreamingResponse:
 
     # 定义 generate_stream 函数
     def generate_stream(params):
-        prompt = params['prompt']
+        prompt = params["prompt"]
         temperature = params.get("temperature", 1.0)
         top_p = params.get("top_p", 1.0)
         max_new_tokens = params.get("max_new_tokens", 256)
 
-        input_ids = glm_tokenizer([prompt], return_tensors='pt').to(device)
+        input_ids = glm_tokenizer([prompt], return_tensors="pt").to(device)
         streamer = TokenStreamer(skip_prompt=True)
         thread = Thread(
             target=model.generate,
@@ -116,8 +121,8 @@ async def inference(request: Request) -> StreamingResponse:
                 max_new_tokens=int(max_new_tokens),
                 temperature=float(temperature),
                 top_p=float(top_p),
-                streamer=streamer
-            )
+                streamer=streamer,
+            ),
         )
         thread.start()
         for token_id in streamer:
@@ -132,8 +137,8 @@ async def inference(request: Request) -> StreamingResponse:
         raise Exception(f"Error during generation: {e}")
 
     # 处理响应标记
-    audio_offset = glm_tokenizer.convert_tokens_to_ids('<|audio_0|>')
-    end_token_id = glm_tokenizer.convert_tokens_to_ids('<|user|>')
+    audio_offset = glm_tokenizer.convert_tokens_to_ids("<|audio_0|>")
+    end_token_id = glm_tokenizer.convert_tokens_to_ids("<|user|>")
     text_tokens = []
     audio_tokens = []
     is_finalize = False
@@ -151,17 +156,23 @@ async def inference(request: Request) -> StreamingResponse:
 
     # 使用 audio_decoder 获取音频数据
     tts_token = torch.tensor(audio_tokens, device=device).unsqueeze(0)
-    tts_speech, _ = audio_decoder.token2wav(tts_token, uuid=this_uuid, prompt_token=flow_prompt_speech_token.to(device),
-                                            prompt_feat=prompt_speech_feat.to(device),finalize=is_finalize)
+    tts_speech, _ = audio_decoder.token2wav(
+        tts_token,
+        uuid=this_uuid,
+        prompt_token=flow_prompt_speech_token.to(device),
+        prompt_feat=prompt_speech_feat.to(device),
+        finalize=is_finalize,
+    )
     # 将音频数据转换为字节
     result = io.BytesIO()
-    torchaudio.save(result, tts_speech.cpu(), 22050, format='wav')
+    torchaudio.save(result, tts_speech.cpu(), 22050, format="wav")
     result.seek(0)
     return StreamingResponse(result, media_type="application/octet-stream")
 
+
 def init():
     global model, glm_tokenizer, audio_decoder, whisper_model, feature_extractor, device
-    device = get_proper_device(model_size_mb)
+    device = "cuda"
 
     # 初始化模型路径
     model_path = "THUDM/glm-4-voice-9b"
@@ -170,20 +181,26 @@ def init():
 
     # 初始化 GLM 模型和分词器
     glm_tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(model_path, trust_remote_code=True).eval().to(device)
+    model = (
+        AutoModel.from_pretrained(model_path, trust_remote_code=True).eval().to(device)
+    )
 
     # 初始化 AudioDecoder
     flow_config = os.path.join(flow_path, "config.yaml")
-    flow_checkpoint = os.path.join(flow_path, 'flow.pt')
-    hift_checkpoint = os.path.join(flow_path, 'hift.pt')
+    flow_checkpoint = os.path.join(flow_path, "flow.pt")
+    hift_checkpoint = os.path.join(flow_path, "hift.pt")
 
-    audio_decoder = AudioDecoder(config_path=flow_config, flow_ckpt_path=flow_checkpoint,
-                                 hift_ckpt_path=hift_checkpoint,
-                                 device=torch.device(f"cuda:{device}"))
+    audio_decoder = AudioDecoder(
+        config_path=flow_config,
+        flow_ckpt_path=flow_checkpoint,
+        hift_ckpt_path=hift_checkpoint,
+        device=torch.device(f"cuda:{device}") if type(device) == int else device,
+    )
 
     # 初始化语音分词器
     whisper_model = WhisperVQEncoder.from_pretrained(tokenizer_path).eval().to(device)
     feature_extractor = WhisperFeatureExtractor.from_pretrained(tokenizer_path)
+
 
 def hangup():
     global model, glm_tokenizer, audio_decoder, whisper_model, feature_extractor
@@ -193,6 +210,7 @@ def hangup():
     del whisper_model
     del feature_extractor
     torch.cuda.empty_cache()
+
 
 if __name__ == "__main__":
     setproctitle.setproctitle("glm4voice-web-inference")
